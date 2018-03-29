8434cb135dca80f4ff2d1044b7b4a04453f3f83d
Rob LeCheminant <lecheminant@gmail.com>
2018-03-29 15:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- // recipes.recipe3 = 'pizza';
-  //return ();
+  
 }
 
 function deleteFromObjectByKey(object, key){
